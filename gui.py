e9abdb7f1c2e8cf54fed8715231067e137a9bade
koji <iisparkplugiiog@yahoo.com>
2020-05-01 16:39:32 +0800

    SUBJECT: GUI w/ GetOldTweets
    BODY: Working get old tweets with gui
    TASK: takes keyword, tweet count, start date and end date
    Notes: Still wip and need to error check

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from tkinter import *
-
+from tkcalendar import *
+from tkinter import messagebox
+import GetOldTweets3 as got
 
 LARGE_FONT = ("Verdana", 12, "bold")
 BUTTON_FONT = ("Verdana", 10)
@@ -59,14 +61,79 @@ class Guide(Frame):
         
     
 class GatherData(Frame):
+    keyword = ''
+    tweetCount = 0
+    startDate = ''
+    endDate = ''
+
     def __init__ (self, parent, controller):
         Frame.__init__(self, parent)
         titleFrame = Frame(self, bg="#4285f4")
         title = Label(titleFrame, text="Gather Data", fg="white", bg="#4285f4", font=LARGE_FONT)
-        self.app_toolbar = Toolbar(self, controller)
-        self.app_toolbar.pack(anchor="nw")
+        app_toolbar = Toolbar(self, controller)
+        app_toolbar.pack(anchor="nw")
         title.pack()
         titleFrame.pack(fill=BOTH)
+        
+        leftFrame = Frame(self)
+        leftFrame.pack(side=LEFT, anchor="nw")
+        keywordLabel = Label(leftFrame, text="Keywords(s) :")
+        tweetCount = Label(leftFrame, text="No. of Tweets :")
+        StartDate = Label(leftFrame, text="Start Date :")
+        EndDate =  Label(leftFrame, text="End Data :")
+        keywordLabel.grid(row=0, column=0)
+        tweetCount.grid(row=1)
+        StartDate.grid(row=2)
+        EndDate.grid(row=3)
+
+        keyword = StringVar()
+        tweetCount = IntVar()
+
+        keywordEntry = Entry(leftFrame, width=25, textvariable=keyword)
+        tweetCount = Entry(leftFrame, width=25, textvariable=tweetCount)
+        DateStart = DateEntry(leftFrame, width=22, background="white")
+        DateEnd = DateEntry(leftFrame, width=22, background="white")
+        DateStart.grid(row=2,column=1)
+        DateEnd.grid(row=3, column=1)
+        tweetCount.grid(row=1, column=1)
+        keywordEntry.grid(row=0, column=1)
+
+        def collect():
+            self.keyword = str(keyword.get())
+            self.tweetCount = int(tweetCount.get())
+            self.startDate = str(DateStart.get_date())
+            self.endDate = str(DateEnd.get_date())
+            self.GetOldTweets(self.keyword, self.tweetCount, self.startDate, self.endDate)
+
+        collectBtn = Button(leftFrame, text="Collect", bg="#4285f4", fg="white", width=21, command=collect)
+        collectBtn.grid(row=4, column=1)
+        clearBtn = Button(leftFrame, text="Clear", bg="#4285f4", fg="white", width=21)
+        clearBtn.grid(row=5, column=1)
+
+    def GetOldTweets(self,keyword, count, start, end):
+        tweetCriteria = got.manager.TweetCriteria()\
+                .setQuerySearch(keyword)\
+                .setSince(start)\
+                .setUntil(end)\
+                .setMaxTweets(count)
+        tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+        text_tweets = [[tweet.text] for tweet in tweets]
+        dataFile = FileHandling('tweets.txt', text_tweets)
+
+class FileHandling:
+    def __init__(self, parent, tweets):
+        self.File = open(parent, "w+", encoding='utf-8')
+        self.handleFile(tweets)
+        self.File.close()
+        
+    def handleFile(self, tweets):
+        text = ""
+        for i in range(0, len(tweets)):
+            text = tweets[i][0] + '\n' + text
+            self.File.write(text)
+
+        
+
 
 app = ProjectApp()
 app.mainloop()
