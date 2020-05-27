55e4251be1d0856aae6a1df8f3ff613a4ff7692c
koji <iisparkplugiiog@yahoo.com>
2020-05-27 20:36:44 +0800

    SUBJECT: WIP Data Process and Cleaning GUI
    BODY: Need handle error and design fix
    TASK:
    Notes:

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from tkcalendar import *
 from tkinter import messagebox
 import GetOldTweets3 as got
@@ -18,7 +19,7 @@ class ProjectApp(Tk):
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
 
-        for F in (Guide, GatherData, Toolbar):
+        for F in (Guide, GatherData, DataPC, Toolbar):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nswe")
@@ -37,7 +38,7 @@ class Toolbar(Frame):
         Frame.bg="#4285f4"
         btn1 = Button(self, text="Guide", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(Guide))
         btn2 = Button(self, text="Gather Data", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(GatherData))
-        btn3 = Button(self, text="Data Processing and Cleaning", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(Guide))
+        btn3 = Button(self, text="Data Processing and Cleaning", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(DataPC))
         btn4 = Button(self, text="Data Training", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(GatherData))
         btn5 = Button(self, text="Classification", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(Guide))
         btn6 = Button(self, text="Visualization", font=BUTTON_FONT, bd=1, bg="#4285f4", fg="white", command=lambda: controller.show_frame(GatherData))
@@ -62,10 +63,10 @@ class Guide(Frame):
     
 class GatherData(Frame):
     keyword = ''
-    tweetCount = 0
+    tweetCount = 0  
     startDate = ''
     endDate = ''
-
+    filename = 'Choose a file'
     def __init__ (self, parent, controller):
         Frame.__init__(self, parent)
         titleFrame = Frame(self, bg="#4285f4")
@@ -74,8 +75,8 @@ class GatherData(Frame):
         app_toolbar.pack(anchor="nw")
         title.pack()
         titleFrame.pack(fill=BOTH)
-        
-        leftFrame = Frame(self)
+    
+        leftFrame = Frame(self, background='red')
         leftFrame.pack(side=LEFT, anchor="nw")
         keywordLabel = Label(leftFrame, text="Keywords(s) :")
         tweetCount = Label(leftFrame, text="No. of Tweets :")
@@ -93,6 +94,11 @@ class GatherData(Frame):
         tweetCount = Entry(leftFrame, width=25, textvariable=tweetCount)
         DateStart = DateEntry(leftFrame, width=22, background="white")
         DateEnd = DateEntry(leftFrame, width=22, background="white")
+        # Initialize Value to none
+        tweetCount.delete(0, "end")
+        DateStart.delete(0, "end")
+        DateEnd.delete(0, "end")
+
         DateStart.grid(row=2,column=1)
         DateEnd.grid(row=3, column=1)
         tweetCount.grid(row=1, column=1)
@@ -105,11 +111,30 @@ class GatherData(Frame):
             self.endDate = str(DateEnd.get_date())
             self.GetOldTweets(self.keyword, self.tweetCount, self.startDate, self.endDate)
 
+        def clearFieldsFn():
+            keywordEntry.delete(0, END)
+            tweetCount.delete(0, END)
+        
+        def handlefile():
+            self.filename = FileHandling.filedialog(self)
+            fileNameDisp = self.filename.split('/')
+            fileNameDisp = fileNameDisp[-1]
+            # fileNameLabel = Label(rightFrame, text=) 
+            fileNameLabel.configure(text=fileNameDisp)
+
         collectBtn = Button(leftFrame, text="Collect", bg="#4285f4", fg="white", width=21, command=collect)
         collectBtn.grid(row=4, column=1)
-        clearBtn = Button(leftFrame, text="Clear", bg="#4285f4", fg="white", width=21)
+        clearBtn = Button(leftFrame, text="Clear", bg="#4285f4", fg="white", width=21, command=clearFieldsFn)
         clearBtn.grid(row=5, column=1)
 
+        rightFrame = Frame(self, background='black')
+        rightFrame.pack(side=RIGHT, anchor="ne")  
+
+        fileNameLabel = Label(rightFrame, text=self.filename)  
+        ChooseFile = Button(rightFrame, text="Choose File", bg="#4285f4", fg="white", width=21, command=handlefile)
+        ChooseFile.pack()
+        fileNameLabel.pack()
+        
     def GetOldTweets(self,keyword, count, start, end):
         tweetCriteria = got.manager.TweetCriteria()\
                 .setQuerySearch(keyword)\
@@ -118,22 +143,54 @@ class GatherData(Frame):
                 .setMaxTweets(count)
         tweets = got.manager.TweetManager.getTweets(tweetCriteria)
         text_tweets = [[tweet.text] for tweet in tweets]
-        dataFile = FileHandling('tweets.txt', text_tweets)
+        FileHandling(self.filename, text_tweets) 
 
+
+class DataPC(Frame):
+    filename = 'Choose a file: '
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.app_toolbar = Toolbar(self, controller)
+        titleFrame = Frame(self, bg="#4285f4")
+        title = Label(titleFrame, text="Data Process and Cleaning", fg="white", bg="#4285f4", font=LARGE_FONT)
+        self.app_toolbar.pack(anchor="nw")
+        title.pack()
+        titleFrame.pack(fill=BOTH)
+
+        self.mainFrame = LabelFrame(self, text="Train a file", width=500, height=250, padx=25, pady=25, background='red')
+        self.mainFrame.pack()  
+        self.filenameLabel = Label(self, text=self.filename)
+        self.filenameLabel.pack()
+        self.Buttons()
+
+    def Buttons(self):
+        def chooseFile():
+            self.filename = FileHandling.filedialog(self)
+            fileNameDisp = self.filename.split('/')
+            fileNameDisp = fileNameDisp[-1]
+            # fileNameLabel = Label(rightFrame, text=) 
+            self.filenameLabel.configure(text=fileNameDisp)
+
+        self.openFileBtn = Button(self.mainFrame, width=21, text = 'Open a File', command=chooseFile)
+        self.openFileBtn.pack()
+
+        
+        
 class FileHandling:
     def __init__(self, parent, tweets):
         self.File = open(parent, "w+", encoding='utf-8')
         self.handleFile(tweets)
         self.File.close()
-        
+
+    def filedialog(self):
+        self.filename = filedialog.askopenfilename(initialdir = "/", title = "Select A File", filetype = (("text", "*.txt"), ("All Files", "*.*")))   
+        return self.filename
+
     def handleFile(self, tweets):
         text = ""
         for i in range(0, len(tweets)):
             text = tweets[i][0] + '\n' + text
             self.File.write(text)
-
         
-
-
 app = ProjectApp()
 app.mainloop()
